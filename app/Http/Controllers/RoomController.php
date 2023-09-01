74b683b26ea01d5a91c3087fbed88e3834ae4d72
doeurnden32 <doeurnden32@gmail.com>
2023-09-01 16:13:21 +0700

    test connecting pgsql

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    public function list(){
+        $rooms = Room::all();
+        return response()->json($rooms);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('rooms')->group(function(){
+    Route::get('/list',[RoomController::class, 'list']);
 });
