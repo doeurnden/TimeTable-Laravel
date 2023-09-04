4ef01575d132ce46e1d90058486337dc8ff1eaf8
doeurnden32 <doeurnden32@gmail.com>
2023-09-05 01:16:31 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+    // public function get_lecturer(){
+    //     return Employee::with(['gender', 'department']); // Change 10 to the desired number of items per page
+    // }
+
+    public function get_lecturer(){
+        return Employee::select('id','name_latin', 'gender_id', 'department_id','id_card')
+        ->with('gender:id,code','department:id,code')
+        ->whereNotNull('gender_id')
+        ->get();
+    }
+}
--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    public function list(){
-        $rooms = Room::all();
-        return response()->json($rooms);
+    public function get_room()
+    {
+        return Room::select('id', 'name', 'nb_desk', 'nb_chair', 'room_type_id', 'building_id')
+            ->with('roomType:id,name','building:id,code')
+            ->orderBy('room_type_id', 'asc')
+            ->orderBy('building_id', 'asc')
+            ->get();
     }
 }
