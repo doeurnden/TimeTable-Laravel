499c7b62a7293a97e5a6e1fca8ac6169dd998d51
thearith99 <athoudomthearith.com>
2023-09-13 08:07:49 +0700

    Update API on group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,23 +7,38 @@ use Illuminate\Support\Facades\DB;
 
 class GroupController extends Controller
 {
-    public function getGroups()
-{
-    $groups = DB::table('studentAnnuals as sa')
-        ->join('students as s', 's.id', '=', 'sa.student_id')
-        ->leftJoin('group_student_annuals as ga', 'ga.student_annual_id', '=', 'sa.id')
-        ->join('groups', 'groups.id', '=', 'ga.group_id')
-        ->select('groups.*') // Select all columns from the "groups" table
-        ->where('sa.academic_year_id', 2023)
-        ->where('sa.grade_id', 4)
-        ->where('sa.department_id', 4)
-        ->where('ga.semester_id', 2)
-        ->whereNull('ga.department_id')
-        ->groupBy('groups.id', /* Add all other columns from the "groups" table here */)
-        ->orderBy('groups.code')
-        ->get();
+    public function getGroups(Request $request)
+    {
+        $groups = DB::table('studentAnnuals as sa')
+            ->join('students as s', 's.id', '=', 'sa.student_id')
+            ->leftJoin('group_student_annuals as ga', 'ga.student_annual_id', '=', 'sa.id')
+            ->join('groups', 'groups.id', '=', 'ga.group_id')
+            ->select('groups.*');
+            if(isset($request->academic_year_id)){
+                $groups->where('sa.academic_year_id',$request->academic_year_id);
+            } 
+            if(isset($request->department_id)){
+                $groups->where('sa.department_id',$request->department_id);
+            }
+            if(isset($request->grade_id)){
+                $groups->where('sa.grade_id',$request->grade_id);
+            }
+            if(isset($request->semester_id)){
+                $groups->where('sa.semester_id',$request->semester_id);
+            }
+            
+            // Select all columns from the "groups" table
+            // ->where('sa.academic_year_id', $request->academic_year_id)
+            // ->where('sa.grade_id', 4)
+            // ->where('sa.department_id', 4)
+            // ->where('ga.semester_id', 2)
+            // ->whereNull('ga.department_id')
+            // ->groupBy('groups.id', /* Add all other columns from the "groups" table here */)
+            $groups->whereNull('ga.department_id')
+            ->orderBy('groups.code');
+            $query = $groups->get();
 
-    return response()->json($groups);
-}
+        return response()->json( $query);
+    }
 
 }
